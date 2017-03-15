0c90ebc0828694fc705ad4ca157a815e244b3ab5
Petar Šlat <petar.slat@gmail.com>
2017-03-15 16:52:02 +0100

    Remove WHP_Recommended and Restart-Computer

--- setup-iis.ps1
+++ setup-iis.ps1
@@ -1,19 +1,9 @@
 Import-Module ServerManager
-
 Add-WindowsFeature Web-Server -includeallsubfeature
-
 Remove-Website -Name "Default Web Site"
 
 Invoke-WebRequest -Uri http://download.microsoft.com/download/C/F/F/CFF3A0B8-99D4-41A2-AE1A-496C08BEB904/WebPlatformInstaller_amd64_en-US.msi -OutFile "WebPlatformInstaller_amd64_en-US.msi"
-
 Start-Process "WebPlatformInstaller_amd64_en-US.msi" /qn -Wait
 
-# WHP_Recommended
-
-Start-Process "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:WHP_Recommended /AcceptEULA" -Wait
-
 # NETFramework452
-
 Start-Process "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:NETFramework452 /AcceptEULA /ForceReboot" -Wait
-
-Restart-Computer
