e2b13759d581b4fe6172c66e4645997a4fab1510
Petar Šlat <petar.slat@gmail.com>
2017-03-15 16:38:50 +0100

    Update the setup-iis script

--- setup-iis.ps1
+++ setup-iis.ps1
@@ -1,23 +1,19 @@
 Import-Module ServerManager
+
 Add-WindowsFeature Web-Server -includeallsubfeature
 
+Remove-Website -Name "Default Web Site"
 
 Invoke-WebRequest -Uri http://download.microsoft.com/download/C/F/F/CFF3A0B8-99D4-41A2-AE1A-496C08BEB904/WebPlatformInstaller_amd64_en-US.msi -OutFile "WebPlatformInstaller_amd64_en-US.msi"
+
 Start-Process "WebPlatformInstaller_amd64_en-US.msi" /qn -Wait
 
+# WHP_Recommended
+
+Start-Process "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:WHP_Recommended /AcceptEULA" -Wait
 
 # NETFramework452
-Start-Process "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:NETFramework452 /AcceptEULA" -Wait
 
-
-# Is this needed?
-# WDeploy36NoSMO
-# Start-Process "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:WDeploy36NoSMO /AcceptEULA" -WindowStyle Hidden -Wait
-
+Start-Process "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:NETFramework452 /AcceptEULA /ForceReboot" -Wait
 
 Restart-Computer
-
-
-
-# TODO: Add User for web-deploy
-# TODO: configure web deploy for that user over some site
