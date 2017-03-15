fb42e4439d4b9026db6a9c1b934cce921369459a
Petar Šlat <petar.slat@gmail.com>
2017-03-15 16:59:37 +0100

    Remove Stop-Website for default web site
    It's been removed in setup-iis script

--- website.ps1
+++ website.ps1
@@ -1,9 +1,5 @@
 Import-Module WebAdministration
 
-Stop-Website -Name "Default Web Site"
-
 New-Item C:\inetpub\www\cats -Type directory
-
 New-Website -Name "Cats" -PhysicalPath "C:\inetpub\www\cats"
-
 Start-Website -Name "Cats"
