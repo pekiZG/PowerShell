1e7a93f797e2413eda0b0a47625e56dfdc7e4bef
Petar Šlat <petar.slat@gmail.com>
2017-03-16 23:26:49 +0100

    Add -ArgumentList to first Start-Process

--- setup-iis.ps1
+++ setup-iis.ps1
@@ -3,7 +3,7 @@ Add-WindowsFeature Web-Server -includeallsubfeature
 Remove-Website -Name "Default Web Site"
 
 Invoke-WebRequest -Uri http://download.microsoft.com/download/C/F/F/CFF3A0B8-99D4-41A2-AE1A-496C08BEB904/WebPlatformInstaller_amd64_en-US.msi -OutFile "WebPlatformInstaller_amd64_en-US.msi"
-Start-Process "WebPlatformInstaller_amd64_en-US.msi" /qn -Wait
+Start-Process "WebPlatformInstaller_amd64_en-US.msi" -ArgumentList "/qn" -Wait
 
 # NETFramework452
 Start-Process "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:NETFramework452 /AcceptEULA /ForceReboot" -WindowStyle Hidden -Wait
