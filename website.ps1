b0e4e48b7ce0035cbc7224f02b1adafc41b3b464
Petar Šlat <petar.slat@gmail.com>
2017-03-16 23:28:06 +0100

    Add checks so script gracefully fails

--- website.ps1
+++ website.ps1
@@ -4,20 +4,34 @@ $physicalPath = "C:\inetpub\www\" + $siteName.ToLower()
 
 Import-Module WebAdministration
 
-New-WebAppPool -Name $siteName
+if (!(Test-Path IIS:\AppPools\$siteName)) {
+    New-WebAppPool -Name $siteName
+}
 
-New-Item $physicalPath -Type directory
-New-Website -Name $siteName -ApplicationPool $siteName -PhysicalPath $physicalPath
-Start-Website -Name $siteName
+if (!(Test-Path $physicalPath)) {
+    New-Item $physicalPath -Type directory
+}
 
-$fileName = Split-Path $uri -leaf
-Invoke-WebRequest -Uri $uri -OutFile $fileName
+if (!(Test-Path IIS:\Sites\$siteName)) {
+    New-Website -Name $siteName -ApplicationPool $siteName -PhysicalPath $physicalPath
+    Start-Website -Name $siteName
+}
 
-# Update 'setup-iis' to to use this
-# https://msdn.microsoft.com/en-us/powershell/wmf/5.1/install-configure
-#Expand-Archive -Path $fileName -DestinationPath $physicalPath -Force
+try {
+    $fileName = Split-Path $uri -leaf
+    Invoke-WebRequest -Uri $uri -OutFile $fileName
+    # Update 'setup-iis' to to use this
+    # https://msdn.microsoft.com/en-us/powershell/wmf/5.1/install-configure
+    #Expand-Archive -Path $fileName -DestinationPath $physicalPath -Force
 
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory((Get-Location).Path + "\" + $fileName, $physicalPath)
-
-# TODO: Switces for Recycle Application Pool and maybe Restart WebSite
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [System.IO.Compression.ZipFile]::ExtractToDirectory((Get-Location).Path + "\" + $fileName, $physicalPath)
+}
+catch {
+    Write-Host "Oops..."
+    exit
+}
+finally {
+    Restart-WebAppPool -Name $siteName
+    Remove-Item (Get-Location).Path + "\" + $fileName
+}
