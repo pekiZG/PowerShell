e27a1cbd3ab9e0d9f578f8db31d9590b08a8cabe
Petar Šlat <petar.slat@gmail.com>
2017-03-24 00:34:45 +0100

    Add WMF 5.1

--- setup-msbuild-tools.ps1
+++ setup-msbuild-tools.ps1
@@ -2,9 +2,17 @@
 (New-Object System.Net.WebClient).DownloadFile("http://download.microsoft.com/download/C/F/F/CFF3A0B8-99D4-41A2-AE1A-496C08BEB904/WebPlatformInstaller_amd64_en-US.msi", (Get-location).Path + "\WebPlatformInstaller_amd64_en-US.msi")
 Start-Process "WebPlatformInstaller_amd64_en-US.msi" -ArgumentList "/qn" -Wait 
  
-# Git for Windows  
-Start-Process "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:NETFramework452,msysgitVS /AcceptEULA" -WindowStyle Hidden -Wait
- 
+# NETFramework452
+Start-Process "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:NETFramework452 /AcceptEULA" -WindowStyle Hidden -Wait
+
+# https://msdn.microsoft.com/en-us/powershell/wmf/5.1/install-configure
+# Windows Management Framework (dependant on NETFramework452)
+(New-Object System.Net.WebClient).DownloadFile("http://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win8.1AndW2K12R2-KB3191564-x64.msu", (Get-location).Path + "\Win8.1AndW2K12R2-KB3191564-x64.msu")
+Start-Process "Win8.1AndW2K12R2-KB3191564-x64.msu" -ArgumentList "/quiet" -Wait
+
+# Git for Windows
+Start-Process "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:msysgitVS /AcceptEULA" -WindowStyle Hidden -Wait
+
 # NuGet
 New-Item "C:\NuGet\" -Type Directory
 (New-Object System.Net.WebClient).DownloadFile("https://dist.nuget.org/win-x86-commandline/v4.0.0/nuget.exe", "C:\NuGet\nuget.exe")
