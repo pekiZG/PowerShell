2dbdfb526e01ed5c8afb84a4bbfed9af449271b2
Petar Šlat <petar.slat@gmail.com>
2017-03-24 00:26:01 +0100

    Add WMF 5.1

--- setup-iis.ps1
+++ setup-iis.ps1
@@ -8,5 +8,8 @@ Start-Process "WebPlatformInstaller_amd64_en-US.msi" -ArgumentList "/qn" -Wait
 # NETFramework452
 Start-Process "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:NETFramework452 /AcceptEULA /ForceReboot" -WindowStyle Hidden -Wait
 
-# TODO: Install Windows Management Framework (dependant on NETFramework452)
+
 # https://msdn.microsoft.com/en-us/powershell/wmf/5.1/install-configure
+# Windows Management Framework (dependant on NETFramework452)
+(New-Object System.Net.WebClient).DownloadFile("http://download.microsoft.com/download/6/F/5/6F5FF66C-6775-42B0-86C4-47D41F2DA187/Win8.1AndW2K12R2-KB3191564-x64.msu", (Get-location).Path + "\Win8.1AndW2K12R2-KB3191564-x64.msu")
+Start-Process "Win8.1AndW2K12R2-KB3191564-x64.msu" -ArgumentList "/quiet" -Wait
