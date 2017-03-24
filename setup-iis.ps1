09c4145ee2b12056a084a72941eecac42106c7c5
Petar Šlat <petar.slat@gmail.com>
2017-03-24 01:49:20 +0100

    SuppressReboot of NETFramework452
    because installation of Windows Management Framework 5.1 will
    Restart-Computer

--- setup-iis.ps1
+++ setup-iis.ps1
@@ -6,7 +6,7 @@ Invoke-WebRequest -Uri http://download.microsoft.com/download/C/F/F/CFF3A0B8-99D
 Start-Process "WebPlatformInstaller_amd64_en-US.msi" -ArgumentList "/qn" -Wait
 
 # NETFramework452
-Start-Process "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:NETFramework452 /AcceptEULA /ForceReboot" -WindowStyle Hidden -Wait
+Start-Process "C:\Program Files\Microsoft\Web Platform Installer\WebpiCmd-x64.exe" -ArgumentList "/Install /Products:NETFramework452 /AcceptEULA /SuppressReboot" -WindowStyle Hidden -Wait
 
 
 # https://msdn.microsoft.com/en-us/powershell/wmf/5.1/install-configure
